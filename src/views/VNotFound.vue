9d7611bc4f928675767bb2ec2d755bf7714ceff4
Alex Gerasimov <alexandr041299@gmail.com>
2022-11-08 10:01:08 +0500

    Create not found page

--- src/views/VNotFound.vue
+++ src/views/VNotFound.vue
@@ -1,15 +1,50 @@
 <template>
-  <div>
-    NOT FOUND
+  <div class="not-found-container">
+    <h1 class="not-found-container__title">
+      PAGE NOT FOUND
+    </h1>
+    <h2 class="not-found-container__subtitle">
+      Error 404
+    </h2>
+    <base-button 
+      class="not-found-container__button" 
+      @click="$router.push('/')"
+    >
+      Вернуться на главную
+    </base-button>
   </div>
 </template>
 
 <script>
+    import BaseButton from "@/components/UI/BaseButton";
     export default {
-        name: "VNotFound"
+        name: "VNotFound",
+      components: {BaseButton}
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.not-found-container {
+  margin: 100px auto;
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  align-content: center;
+  justify-content: center;
+  text-align: center;
+  padding: 25px;
+  &__title {
+    padding: 25px;
+    font-size: 35px;
+  }
+  &__subtitle {
+    padding: 10px;
+    font-size: 25px;
+  }
+  &__button {
+    max-width: 50%;
+    margin: 10px auto;
+  }
+}
 
 </style>
